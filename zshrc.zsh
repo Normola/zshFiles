a36d1c0d23ea2ec5365f551f194f3111f7554ed5
JT <Normola@gmail.com>
2017-12-04 18:08:01 +0000

    Additions

--- zshrc.zsh
+++ zshrc.zsh
@@ -17,3 +17,8 @@ antigen bundle zsh-users/zsh-history-substring-search
 antigen theme robbyrussell/oh-my-zsh themes/agnoster
 
 antigen apply
+
+export PATH=/opt/platform-tools:$PATH
+
+alias bat='upower -i /org/freedesktop/UPower/devices/battery_BAT0| grep -E "state|to\ full|percentage"'
+
