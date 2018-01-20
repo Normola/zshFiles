ed17c928cfe247c80eb5545ec987aba62a5ba10f
JT <JT@undergroundBunker.co.uk>
2018-01-20 08:07:09 +0000

    Added nvm.  Added git ignore

--- zshrc.zsh
+++ zshrc.zsh
@@ -1,3 +1,5 @@
+export NVM_DIR="$HOME/.custom-nvm-dir"
+
 source ~/zsh/antigen/antigen.zsh
 
 antigen use oh-my-zsh
@@ -5,6 +7,7 @@ antigen use oh-my-zsh
 antigen bundle command-not-found
 antigen bundle extract
 antigen bundle git
+antigen bundle heroku
 antigen bundle pip
 
 antigen bundle unixorn/autoupdate-antigen.zshplugin
@@ -13,6 +16,8 @@ antigen bundle zsh-users/zsh-completions
 antigen bundle zdharma/fast-syntax-highlighting
 antigen bundle zsh-users/zsh-history-substring-search
 #antigen bundle zsh-users/zsh-syntax-highlighting
+
+antigen bundle lukechilds/zsh-nvm
 
 antigen theme robbyrussell/oh-my-zsh themes/agnoster
 
