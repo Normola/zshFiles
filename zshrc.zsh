44b8dfeb5149d34790bfdfe21a1a499c4d39d490
JT <JT@UndergroundBunker.co.uk>
2018-09-27 05:31:56 +0100

    Removed old stuff.  Added docker

--- zshrc.zsh
+++ zshrc.zsh
@@ -7,7 +7,6 @@ source ~/zsh/antigen/antigen.zsh
 
 antigen use oh-my-zsh
 
-antigen bundle archlinux
 antigen bundle aws
 antigen bundle command-not-found
 antigen bundle common-aliases
@@ -17,7 +16,6 @@ antigen bundle heroku
 antigen bundle history
 antigen bundle pip
 antigen bundle ssh-agent
-antigen bundle sudo
 antigen bundle systemd
 
 antigen bundle arialdomartini/oh-my-git #Cool git theme
@@ -37,16 +35,9 @@ antigen theme robbyrussell/oh-my-zsh themes/agnoster
 
 antigen apply
 
-if [ -d "/opt/platform-tools" ] ; then
-	export PATH=/opt/platform-tools:$PATH
-fi
-
-if [ -d "$HOME/Dev/AndroidDev/bin" ] ; then
-	export PATH=$MYHOME/Dev/AndroidDev/bin:$PATH
-	export PATH=$MYHOME/Dev/AndroidDev/bootTools:$PATH
-fi
-
 export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/snap/bin:$PATH
-expott PATH=/home/jim/kube/client/bin:$PATH
 alias bat='upower -i /org/freedesktop/UPower/devices/battery_BAT0| grep -E "state|to\ full|percentage"'
 
+alias sudo='sudo '
+alias d='sudo docker'
+alias dc='sudo docker-compose'
